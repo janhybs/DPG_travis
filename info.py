f529d8f1da9b4b05a0e8d1bff053e9f1bb001273
Jan Hybš <x3mSpeedy@users.noreply.github.com>
2017-04-18 10:37:28 +0200

    Add coverage demonstration

--- info.py
+++ info.py
@@ -3,3 +3,13 @@ import sys
 
 print("Python version: {version}".format(version=sys.version))
 print("Total cores: {cores}".format(cores=multiprocessing.cpu_count()))
+
+
+if True:
+  print("This will be covered")
+else:
+  print("This will NOT be covered and may contain some errors")
+  print("For example division by zero")
+  a = 1
+  b = 0
+  c = a / b
